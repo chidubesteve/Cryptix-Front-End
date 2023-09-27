4b16375cb5ce7fbc5da8c16ea5b64d30530483c3
Chidube Anike <117487194+chidubesteve@users.noreply.github.com>
2023-09-27 14:09:32 +0100

    file structure and added images

---
+++ components/NavBar/NavBar.jsx
@@ -0,0 +1,19 @@
+import React, {useState, useEffect} from 'react'
+
+// Import icons
+import {MdNotifications} from 'react-icons/md'
+import {BsSearch} from 'react-icons/bs'
+import {CgMenuLeft, CgMenuRight} from 'react-icons/cg'
+
+// internal imports
+import  styles from './NavBar.module.css'
+import {Discover, HelpCenter, Notification, Profile, SideBar} from './index'
+import { Button } from '../ComponentIndex'
+
+const NavBar = () => {
+  return (
+    <div>NavBar</div>
+  )
+}
+
+export default NavBar
