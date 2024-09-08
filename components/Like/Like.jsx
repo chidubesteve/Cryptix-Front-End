484850b89ff466d999f3050bbc5b93b9ceae13c1
chidubesteve <anikechidube@gmail.com>
2024-09-09 00:47:33 +0100

    increased range for onclick function for better accessibility and ux

--- components/Like/Like.jsx
+++ components/Like/Like.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import { AiFillHeart, AiOutlineHeart } from 'react-icons/ai';
+import React from "react";
+import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
 
 //internal imports
-import style from './Like.module.css';
+import style from "./Like.module.css";
 
 const Like = ({ isLiked, noOfLikes, handleClick }) => {
   return (
-    <div className={style.like_box}>
+    <div className={style.like_box} onClick={() => handleClick()}>
       {isLiked ? (
         <AiFillHeart
           className={style.like_icon}
@@ -14,7 +14,7 @@ const Like = ({ isLiked, noOfLikes, handleClick }) => {
           onClick={() => handleClick()}
         />
       ) : (
-        <AiOutlineHeart className={style.like_icon} onClick={() => handleClick()} />
+        <AiOutlineHeart className={style.like_icon} />
       )}
       <span>{noOfLikes}</span>
     </div>
