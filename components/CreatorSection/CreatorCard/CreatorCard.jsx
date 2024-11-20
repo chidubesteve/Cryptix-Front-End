bca81dfafaab3e42e7ea2f2b5db976738c2a8d07
chidube anike <anikechidube@gmail.com>
2024-11-20 23:35:57 +0100

    created the creators section

---
+++ components/CreatorSection/CreatorCard/CreatorCard.jsx
@@ -0,0 +1,112 @@
+import React, { useState } from "react";
+
+// internal imports
+import style from "./CreatorCard.module.css";
+import Image from "next/future/image";
+
+const CreatorCard = ({ creatorPic, backgroundImage }) => {
+  const [isFollowing, setIsFollowing] = useState(false);
+
+  const handleFollow = () => {
+    setIsFollowing(!isFollowing);
+  };
+  return (
+    <div className={style.creatorCard}>
+      <div className={style.creatorCard_box}>
+        <div className={style.creatorCard_box_img_box}>
+          <div className={style.creatorCard_box_img}>
+            <Image
+              src={backgroundImage}
+              alt="creator background"
+              width={300}
+              height={98}
+              className={style.creatorCard_box_img_img}
+              placeholder="data:image/ ..."
+              quality={90}
+            />
+          </div>
+        </div>
+
+        <div className={style.creatorCard_box_info}>
+          <div className={style.creatorCard_box_info_svg_box}>
+            <div style={{ position: "relative" }}>
+              {" "}
+              <svg
+                width="134"
+                height="54"
+                viewBox="0 0 134 54"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                className={style.creatorCard_box_info_svg_box_svg}
+              >
+                <path
+                  d="M101.734 19.8581C99.2658 17.4194 96.9737 14.8065 94.5052 12.1935C94.1526 11.671 93.6237 11.3226 93.0947 10.8C92.7421 10.4516 92.5658 10.2774 92.2131 9.92903C85.6895 3.83226 76.6974 0 67 0C57.3026 0 48.3105 3.83226 41.6105 9.92903C41.2579 10.2774 41.0816 10.4516 40.7289 10.8C40.2 11.3226 39.8474 11.671 39.3184 12.1935C36.85 14.8065 34.5579 17.4194 32.0895 19.8581C23.2737 28.7419 11.4605 30.4839 -0.176331 30.8323V54H16.3974H32.0895H101.558H110.197H134V30.6581C122.363 30.3097 110.55 28.7419 101.734 19.8581Z"
+                  fill="currentColor"
+                ></path>
+              </svg>
+              <div className={style.creatorCard_box_info_user_avatar_box}>
+                <Image
+                  src={creatorPic}
+                  alt="creator"
+                  width={100}
+                  height={100}
+                  placeholder="data:image/ ..."
+                  className={style.creatorCard_box_info_user_avatar_box_avatar}
+                />{" "}
+              </div>
+            </div>
+          </div>
+          <div className={style.creatorCard_box_info_info_box}>
+            <div className={style.creatorCard_box_info_info_box_name}>
+              <h3>
+                Obi Chris{" "}
+                <span style={{ marginLeft: "0.25rem" }}>
+                  {" "}
+                  <svg
+                    stroke="currentColor"
+                    fill="currentColor"
+                    strokeWidth="0"
+                    viewBox="0 0 24 24"
+                    color="#38BDF8"
+                    style={{ color: "#38BDF8" }}
+                    height="17"
+                    width="17"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path fill="none" d="M0 0h24v24H0z"></path>
+                    <path d="M23 12l-2.44-2.79.34-3.69-3.61-.82-1.89-3.2L12 2.96 8.6 1.5 6.71 4.69 3.1 5.5l.34 3.7L1 12l2.44 2.79-.34 3.7 3.61.82L8.6 22.5l3.4-1.47 3.4 1.46 1.89-3.19 3.61-.82-.34-3.69L23 12z"></path>
+                    {/* <!-- Checkmark path with white fill --> */}
+                    <path
+                      d="M10.09 16.72l-3.8-3.81 1.48-1.48 2.32 2.33 5.85-5.87 1.48 1.48-7.33 7.35z"
+                      fill="#FFFFFF"
+                    ></path>
+                  </svg>
+                </span>
+              </h3>
+              <div className={style.creatorCard_box_info_info_box_price_box}>
+                {" "}
+                <span className={style.creatorCard_box_info_info_box_price}>
+                  12.521 <span style={{ color: "gray" }}>ETH</span>
+                </span>
+              </div>
+            </div>
+
+            <button
+              className={style.creatorCard_box_info_info_box_follow_box_btn}
+              onClick={handleFollow}
+              style={{
+                background: isFollowing ? "transparent" : "",
+                color: isFollowing ? "#374151" : "",
+                outline: isFollowing && "none",
+              }}
+            >
+              {isFollowing ? "Following" : "Follow"}
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CreatorCard;
---
+++ components/CreatorSection/CreatorSection.jsx
@@ -0,0 +1,115 @@
+import React, { useState } from "react";
+import { RiUserFollowLine } from "react-icons/ri";
+import { MdOutlineNewReleases } from "react-icons/md";
+
+// internal imports
+import style from "./CreatorSection.module.css";
+import { Title } from "../ComponentIndex";
+import { LuUserPlus } from "react-icons/lu";
+import CreatorCard from "./CreatorCard/CreatorCard";
+import images from "../../images";
+
+const CreatorSection = () => {
+  // use a single state to manage selected tab
+  const [selectedTab, setSelectedTab] = useState("popular");
+
+  const handleSelectedTab = (tab) => {
+    setSelectedTab(tab);
+  };
+
+  return (
+    <div className={style.creator}>
+      <div className={style.creator_title}>
+        <Title title="Top List Creators." />
+      </div>
+      <nav className={style.creator_tabs}>
+        <ul className={style.creator_tabs_list}>
+          <li onClick={() => handleSelectedTab("popular")}>
+            <button
+              className={`${style.creator_tabs_list_btn} ${
+                selectedTab === "popular" && style.active
+              }`}
+            >
+              <RiUserFollowLine
+                style={{
+                  width: "1.3rem",
+                  height: "50%",
+                  marginRight: ".625rem",
+                }}
+                className={style.tabIcons}
+              />
+              Popular
+            </button>
+          </li>
+          <li onClick={() => handleSelectedTab("following")}>
+            {" "}
+            <button
+              className={`${style.creator_tabs_list_btn} ${
+                selectedTab === "following" && style.active
+              }`}
+            >
+              <LuUserPlus
+                style={{
+                  width: "1.25rem",
+                  height: "50%",
+                  marginRight: ".625rem",
+                  fontWeight: "600",
+                }}
+                className={style.tabIcons}
+              />
+              Following
+            </button>
+          </li>
+          <li onClick={() => handleSelectedTab("new&NoteWorthy")}>
+            {" "}
+            <button
+              className={`${style.creator_tabs_list_btn} ${
+                selectedTab === "new&NoteWorthy" && style.active
+              }`}
+            >
+              <MdOutlineNewReleases
+                style={{
+                  width: "1.3rem",
+                  height: "50%",
+                  marginRight: ".625rem",
+                }}
+                className={style.tabIcons}
+              />
+              New & Noteworthy{" "}
+            </button>
+          </li>
+        </ul>
+      </nav>
+
+      {/* grid section */}
+      <div className={style.creator_card_grid}>
+        {selectedTab === "popular" &&
+          Array.from({ length: 8 }).map((_, index) => (
+            <CreatorCard
+              key={index}
+              backgroundImage={images[`creatorbackground${index + 1}`]}
+              creatorPic={images[`user${index + 1}`]}
+            />
+          ))}
+        {selectedTab === "following" &&
+          Array.from({ length: 8 }).map((_, index) => (
+            <CreatorCard
+              key={index}
+              backgroundImage={images[`creatorbackground${index + 1}`]}
+              creatorPic={images[`user${index + 1}`]}
+            />
+          ))}
+        {selectedTab === "new&NoteWorthy" &&
+          Array.from({ length: 8 }).map((_, index) => (
+            <CreatorCard
+              key={index}
+              backgroundImage={images[`creatorbackground${index + 1}`]}
+              creatorPic={images[`user${index + 1}`]}
+            />
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default CreatorSection;
