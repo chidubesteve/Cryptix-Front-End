eac4e9c1301003ce90f329d5172cf37a6247cb25
chidube anike <anikechidube@gmail.com>
2024-11-27 14:23:20 +0100

    configured images to use webp and avif formats

--- next.config.js
+++ next.config.js
@@ -2,7 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  experimental: { images: { allowFutureImage: true } },
+  experimental: {
+    images: { allowFutureImage: true, formats: ["image/webp", "image/avif"] },
+  },
 };
 
 module.exports = nextConfig;
