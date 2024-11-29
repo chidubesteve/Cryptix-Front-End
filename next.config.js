6a22fd978191e4a2e539789525be10264e0e7f98
chidube anike <anikechidube@gmail.com>
2024-11-29 02:57:31 +0100

    added file-loader webpack and configured it to load all audio file types

--- next.config.js
+++ next.config.js
@@ -8,6 +8,22 @@ const nextConfig = {
   experimental: {
     images: { allowFutureImage: true },
   },
+   webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(mp3|wav|ogg)$/, // Match audio file types
+      use: [
+        {
+          loader: "file-loader",
+          options: {
+            publicPath: "/_next/static/media", // Public path for served files
+            outputPath: "static/media", // Output directory
+            name: "[name].[hash].[ext]", // Naming pattern
+          },
+        },
+      ],
+    });
+    return config;
+  },
 };
 
 module.exports = nextConfig;
