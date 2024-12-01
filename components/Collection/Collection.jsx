481bf30e260c14cb7d3c30c9dac758105bc30822
chidube anike <anikechidube@gmail.com>
2024-12-01 12:08:41 +0100

    fixed responsiveness issue

--- components/Collection/Collection.jsx
+++ components/Collection/Collection.jsx
@@ -42,25 +42,20 @@ const Collection = () => {
     slidesToScroll: 1,
     responsive: [
       {
-        breakpoint: 1024,
+        breakpoint: 1280,
         settings: {
           slidesToShow: 2,
           slidesToScroll: 1,
         },
       },
       {
-        breakpoint: 700,
-        settings: {
-          slidesToShow: 2,
-        },
-      },
-      {
-        breakpoint: 480,
+        breakpoint: 1023,
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
         },
       },
+
     ],
   };
 
