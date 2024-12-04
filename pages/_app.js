47fed29287a2061c5289877e2bc278eb1574b06c
chidube anike <anikechidube@gmail.com>
2024-12-04 03:47:39 +0100

    wrap ThemeProvider around app

--- pages/_app.js
+++ pages/_app.js
@@ -2,25 +2,52 @@ import "../styles/globals.css";
 import Head from "next/head";
 // internal import
 import { NavBar, Footer } from "../components/ComponentIndex";
+import { ThemeProvider } from "../utils/ThemeProvider";
 
 const MyApp = ({ Component, pageProps }) => (
   <>
     <Head>
-      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-      <link rel="manifest" href="/site.webmanifest"/>
-      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5"  / >
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
       <meta name="msapplication-TileColor" content="#2b5797" />
       <meta name="theme-color" content="#ffffff" />
       // set title an description for the site
       <title>Cryptix &nbsp;&mdash;&nbsp; NFT Marketplace</title>
-      <meta name="description" content="The world's first and largest digital marketplace for crypto collectibles and non-fungible token (NFTs). Buy, sell, and discover exclusive digital items" />
+      <meta
+        name="description"
+        content="The world's first and largest digital marketplace for crypto collectibles and non-fungible token (NFTs). Buy, sell, and discover exclusive digital items"
+      />
     </Head>
-    <NavBar />
-    <Component {...pageProps} />
-    <Footer />
+
+    <ThemeProvider
+      defaultTheme="light"
+      enableSystem
+      disableTransitionOnChange
+      suppressHydrationWarning={true}
+    >
+      <NavBar />
+      <Component {...pageProps} />
+      <Footer />
+    </ThemeProvider>
   </>
 );
 
 export default MyApp;
+
