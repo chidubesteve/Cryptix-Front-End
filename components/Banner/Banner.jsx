451933e03ef90e5c87490a3fa4c77f4b28815019
chidube anike <anikechidube@gmail.com>
2024-12-23 12:35:31 +0100

    modified to render background color if no image is provided

--- components/Banner/Banner.jsx
+++ components/Banner/Banner.jsx
@@ -2,19 +2,33 @@ import Image from "next/image";
 import React from "react";
 
 import styles from "./Banner.module.css";
+import { useTheme } from "next-themes";
 
 const Banner = ({ bannerImg }) => {
+
+  const {resolvedTheme} = useTheme()
   return (
     <div className={styles.banner}>
-      <div className={styles.banner_box}>
-        <Image
-          src={bannerImg}
-          alt="banner"
-          width={1620}
-          height={350}
-          placeholder="blur"
-          className={styles.banner_img}
-        />
+      <div
+        className={styles.banner_box}
+        style={{
+          backgroundColor: !bannerImg
+            ? resolvedTheme === "dark"
+              ? "#0e131f"
+              : "#fef2f8"
+            : "transparent", // Transparent if bannerImg is provided
+        }}
+      >
+        {bannerImg && (
+          <Image
+            src={bannerImg}
+            alt="banner"
+            width={1620}
+            height={350}
+            placeholder="blur"
+            className={styles.banner_img}
+          />
+        )}
       </div>
     </div>
   );
