e6ecf4caeeb50e0c2eef1eeff10b76839b02932c
chidube anike <anikechidube@gmail.com>
2025-01-25 23:00:39 +0100

    option to modify style of p tag

--- components/Title/Title.jsx
+++ components/Title/Title.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 // internal imports
 import style from "./Title.module.css";
-const Title = ({ title, paragraph }) => {
+const Title = ({ title, paragraph, p_style }) => {
   return (
     <div className={style.title}>
       <div className={style.title_box}>
         <h2>{title}</h2>
-        {paragraph && <p>{paragraph}</p>}
+        {paragraph && <p style={ p_style }>{paragraph}</p>}
       </div>
     </div>
   );
