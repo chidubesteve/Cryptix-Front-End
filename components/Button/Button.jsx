4e52fd19a0843c9f0b8f02d594c1df6dc67412d5
chidube anike <anikechidube@gmail.com>
2025-01-25 22:56:34 +0100

    added type parameter

--- components/Button/Button.jsx
+++ components/Button/Button.jsx
@@ -4,7 +4,7 @@ import { useRouter } from "next/router";
 // internal import
 import style from "./Button.module.css";
 
-const Button = forwardRef(({ btnName, handleClick, className, icon, href }, ref) => {
+const Button = forwardRef(({ type="button", btnName, handleClick, className, icon, href }, ref) => {
   const router = useRouter();
 
   const onClickHandler = () => {
@@ -18,7 +18,7 @@ const Button = forwardRef(({ btnName, handleClick, className, icon, href }, ref)
 
   const classNames = `${style.button} ${className ? className : ""}`;
   return (
-    <button ref={ref} className={classNames} onClick={onClickHandler}>
+    <button ref={ref} className={classNames} onClick={onClickHandler} type={type}>
       {btnName} {icon && icon}
     </button>
   );
