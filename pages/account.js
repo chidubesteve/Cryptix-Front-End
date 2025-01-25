baefd1af4767a0a0837093761af7ceca47ca1ac8
chidube anike <anikechidube@gmail.com>
2025-01-25 22:56:59 +0100

    added form

--- pages/account.js
+++ pages/account.js
@@ -29,34 +29,47 @@ const account = () => {
             style.dark_account_page_demarcation_border
           }`}
         ></div>
-
-        <div className={style.account_page_box_form_box}>
-          <div
-            className={style.account_page_box_form_box_profile_pic_upload_box}
-          >
-            <Image
-              src={images.user9}
-              width={150}
-              height={150}
-              alt="user 9"
-              className={
-                style.account_page_box_form_box_profile_pic_upload_box_img
-              }
-            />
+        <form action="/update-profile" method="post">
+          <div className={style.account_page_box_form_box}>
             <div
-              className={
-                style.account_page_box_form_box_profile_pic_upload_box_btn
-              }
+              className={style.account_page_box_form_box_profile_pic_upload_box}
             >
-              <input type="file" name="Choose a file" id="file_upload_button" className={style.account_page_box_form_box_profile_pic_upload_box_btn_input} accept="image/*"/>
-              <BiImageAdd className={style.account_page_box_form_box_profile_pic_upload_box_btn_icon} />
-              <span>Change photo</span>
+              <Image
+                src={images.user9}
+                width={150}
+                height={150}
+                alt="user 9"
+                className={
+                  style.account_page_box_form_box_profile_pic_upload_box_img
+                }
+              />
+              <div
+                className={
+                  style.account_page_box_form_box_profile_pic_upload_box_btn
+                }
+              >
+                <input
+                  type="file"
+                  name="Choose a file"
+                  id="file_upload_button"
+                  className={
+                    style.account_page_box_form_box_profile_pic_upload_box_btn_input
+                  }
+                  accept="image/*"
+                />
+                <BiImageAdd
+                  className={
+                    style.account_page_box_form_box_profile_pic_upload_box_btn_icon
+                  }
+                />
+                <span>Change photo</span>
+              </div>
+            </div>
+            <div className={style.account_page_box_form_box_form}>
+              <Form />
             </div>
           </div>
-          <div className={style.account_page_box_form_box_form}>
-            <Form />
-          </div>
-        </div>
+        </form>
       </div>
     </div>
   );
