34e19309f6cd02c9d01814422f0f7b3940dad2b3
chidube anike <anikechidube@gmail.com>
2025-03-18 03:30:51 +0100

    fixed next themes by Passing props correctly and spreading props when being accepted as a parameter

--- utils/ThemeProvider.js
+++ utils/ThemeProvider.js
@@ -1,7 +1,7 @@
 "use client";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 
-export function ThemeProvider({ children, props }) {
+export function ThemeProvider({ children, ...props }) {
   return (
     <NextThemesProvider {...props}>
       {children}
